9104d73348846244616a055c08de07693863f15b
Francois Roland <froland@users.noreply.github.com>
2022-05-13 08:55:15 +0000

    Generate project structure. https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.6.7&packaging=jar&jvmVersion=11&groupId=be.eafcuccle.projint&artifactId=backend-froland&name=backend-froland&description=Demo%20project%20for%20Spring%20Boot&packageName=be.eafcuccle.projint.backendfroland&dependencies=web,actuator,devtools,data-jpa,postgresql,h2,flyway

---
+++ src/main/java/be/eafcuccle/projint/backendfroland/BackendFrolandApplication.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.projint.backendfroland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFrolandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFrolandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/eafcuccle/projint/backendfroland/BackendFrolandApplicationTests.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.projint.backendfroland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFrolandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
