795acfaa4c76990050ddccd8a481a47aa77eb031
Francois ROLAND <froland@froland.be>
2022-05-14 16:44:05 +0000

    Removed unused imports.

--- src/main/java/be/eafcuccle/projint/backendfroland/persistence/AcademicYear.java
+++ src/main/java/be/eafcuccle/projint/backendfroland/persistence/AcademicYear.java
@@ -5,7 +5,6 @@ import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
--- src/main/java/be/eafcuccle/projint/backendfroland/persistence/Course.java
+++ src/main/java/be/eafcuccle/projint/backendfroland/persistence/Course.java
@@ -2,7 +2,6 @@ package be.eafcuccle.projint.backendfroland.persistence;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
