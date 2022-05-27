460b0b3b663c2fe323f4dda449859689840ddc06
Francois ROLAND <froland@froland.be>
2022-05-27 14:01:44 +0000

    Allow cross-origin requests.

--- src/main/java/be/eafcuccle/projint/backendfroland/rest/SectionController.java
+++ src/main/java/be/eafcuccle/projint/backendfroland/rest/SectionController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import be.eafcuccle.projint.backendfroland.persistence.Section;
 import be.eafcuccle.projint.backendfroland.persistence.SectionRepository;
 
 @RestController
+@CrossOrigin
 public class SectionController {
   @Autowired
   private SectionRepository sectionRepository;
