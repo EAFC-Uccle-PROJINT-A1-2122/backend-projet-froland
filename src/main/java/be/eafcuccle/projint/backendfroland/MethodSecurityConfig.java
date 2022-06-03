403493d62e1a8a3dc2cb7f7251c3899df11e88c7
Francois ROLAND <froland@froland.be>
2022-06-03 07:47:04 +0000

    Clean up.

--- src/main/java/be/eafcuccle/projint/backendfroland/MethodSecurityConfig.java
+++ src/main/java/be/eafcuccle/projint/backendfroland/MethodSecurityConfig.java
@@ -1,6 +1,5 @@
 package be.eafcuccle.projint.backendfroland;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
